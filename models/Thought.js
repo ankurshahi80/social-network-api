dae80cfb1b3e1d6059bd3aae4868fdca571155f1
Ankur <ankurshahi@Ankurs-iMac.local>
2021-08-22 16:51:50 -0400

    thought model created

---
+++ models/Thought.js
@@ -0,0 +1,67 @@
+const { Schema, model, Types } = require('mongoose'); // import the Schema constructor and model function
+const dateFormat = require('../utils/dateFormat'); // import the dateFormat function
+// create the Reaction schema
+const ReactionSchema = new Schema(
+    {
+        reactionId:{
+            type:Schema.Types.ObjectId,
+            defalut:() => new Types.ObjectId()
+        },
+        reactionBody: {
+            type:String,
+            required: TextTrackCueList,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default:Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON:{
+            getters:true
+        }
+    }
+);
+
+// create the thought schema
+const ThoughtSchema = new Schema(
+    {
+        thoughtText:{
+            type: String,
+            required: true,
+            minLength: [1, "must include text"],
+            max:[280, "too long. Please reduce to less than 280 characters!"]
+        },
+        createdAt:{
+            type:Date,
+            default:Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions:[reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters:true
+        }
+    }
+);
+ThoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+});
+
+// create the Thought model using ThoughtSchema
+const Thought = model('Thought',ThoughtSchema);
+
+// export the Thought model
+module.exports = Thought;

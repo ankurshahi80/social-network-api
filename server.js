ab161a0c6e2e5bd4e20110c40b5812c4aedd45bc
Ankur <ankurshahi@Ankurs-iMac.local>
2021-08-22 13:56:09 -0400

    added and instantiated express.js server

---
+++ server.js
@@ -0,0 +1,8 @@
+// create an express application
+const express = require('express');
+const app = express();// instantiate the server
+
+
+
+
+app.listen(3001, () => console.log('🌍 Connected on localhost:3001'));//listen for connections on port 3001
